bb0f66d6b58b98c3f30ff8be8ffa5c4f3019ef35
Arham <69010362+ManLykArham@users.noreply.github.com>
2025-02-13 19:09:00 +0000

    Update foodPage.tsx

--- src/app/components/foodPage.tsx
+++ src/app/components/foodPage.tsx
@@ -643,19 +643,19 @@ function FoodPage() {
                         {`${entry.mealName} - ${entry.mealType} - logged at ${entry.timestamp} on ${new Date(entry.date).toLocaleDateString("en-GB")}`}
                       </h1>
                       <div className="bg-blue-100 p-2 border rounded-lg">
-                        <p>Calories: {entry.calories !== undefined ? `${entry.calories} kcal` : "N/A"}</p>
-                        <p>Serving Size: {entry.serving_size_g !== undefined ? `${entry.serving_size_g}g` : "N/A"}</p>
+                        <p>Calories: `{entry.calories} kcal` || "N/A"</p>
+                        <p>Serving Size: `{entry.serving_size_g}g` || N/A</p>
                         <p>
-                          Fat: {entry.fat_total_g !== undefined ? `${entry.fat_total_g}g` : "N/A"} 
-                          (Saturated: {entry.fat_saturated_g !== undefined ? `${entry.fat_saturated_g}g` : "N/A"})
+                          Fat: `{entry.fat_total_g}g ` || "N/A " (Saturated:{" "}
+                          `{entry.fat_saturated_g}g` || "N/A")
                         </p>
-                        <p>Protein: {entry.protein_g !== undefined ? `${entry.protein_g}g` : "N/A"}</p>
-                        <p>Sodium: {entry.sodium_mg !== undefined ? `${entry.sodium_mg}mg` : "N/A"}</p>
-                        <p>Potassium: {entry.potassium_mg !== undefined ? `${entry.potassium_mg}mg` : "N/A"}</p>
-                        <p>Cholesterol: {entry.cholesterol_mg !== undefined ? `${entry.cholesterol_mg}mg` : "N/A"}</p>
+                        <p>Protein: `{entry.protein_g}g` || "N/A"</p>
+                        <p>Sodium: `{entry.sodium_mg}mg` || "N/A"</p>
+                        <p>Potassium: `{entry.potassium_mg}mg` || "N/A"</p>
+                        <p>Cholesterol: `{entry.cholesterol_mg}mg` || "N/A"</p>
                         <p>
-                          Carbohydrates: {entry.carbohydrates_total_g !== undefined ? `${entry.carbohydrates_total_g}g` : "N/A"} 
-                          (Fiber: {entry.fiber_g !== undefined ? `${entry.fiber_g}g` : "N/A"}, Sugar: {entry.sugar_g !== undefined ? `${entry.sugar_g}g` : "N/A"})
+                          Carbohydrates: `{entry.carbohydrates_total_g}g ` || "N/A " (Fiber:{" "}
+                          `{entry.fiber_g}g ` || "N/A ", Sugar: `{entry.sugar_g}g` || "N/A")
                         </p>
                         <button
                           onClick={() => deleteFoodEntry(entry.id)}
